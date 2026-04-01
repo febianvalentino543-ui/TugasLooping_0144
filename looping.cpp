2bf30371898cf1ca10ef1a305d5d274debac08f0
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-01 08:55:15 +0700

    menambahkan fungsi if while cek bilangan prima

--- looping.cpp
+++ looping.cpp
@@ -6,3 +6,12 @@ int n, pilihan;
 
 // Function cek bilangan prima
 bool isPrima(int n){
+    if(n <= 1) return false;
+    int i = 2;
+    while(i <= n/2){
+        if(n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
+
