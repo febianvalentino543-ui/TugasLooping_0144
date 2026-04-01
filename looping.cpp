c56ec600a578da325d5ea89458ad10c6d2df21ed
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-01 09:22:21 +0700

    menambahkan penanganan pilihan tidak valid pada menu

--- looping.cpp
+++ looping.cpp
@@ -74,3 +74,8 @@ int main(){
             case 0:
                 cout << "Program selesai.\n";
                 return 0;
+            default:
+                cout << "Pilihan tidak valid!\n";
+        }
+    }
+}
